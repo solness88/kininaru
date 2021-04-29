c7dbdb9fbe4a633853b1e4851fac9ecc62de4563
hirokazu kobayashi <solness.stf@gmail.com>
2021-04-29 21:51:03 +0900

    News APIからニュース一覧を実装

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,7 @@
+class NewsController < ApplicationController
+  def index
+    #require 'news-api'
+    newsapi = News.new("840742eb9a874490b37a04e08b45c848")
+    @top_headlines = newsapi.get_top_headlines(language: 'en')
+  end
+end
---
+++ db/migrate/20210429125028_create_news.rb
@@ -0,0 +1,6 @@
+class CreateNews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :news do |t|
+    end
+  end
+end
