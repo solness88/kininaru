9d42e74e830048c5bf16dfa9e70264563b9c4778
hirokazu kobayashi <solness.stf@gmail.com>
2021-05-01 09:48:01 +0900

    button_toでparameterをcreateアクションに送ってデータベースに保存

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,4 +4,9 @@ class NewsController < ApplicationController
     newsapi = News.new("840742eb9a874490b37a04e08b45c848")
     @top_headlines = newsapi.get_top_headlines(language: 'en')
   end
+
+  def create
+    New.create(title: params[:title], link: params[:link], image: params[:image])
+    redirect_to news_index_path
+  end
 end
--- db/migrate/20210429125028_create_news.rb
+++ db/migrate/20210429125028_create_news.rb
@@ -1,6 +1,9 @@
 class CreateNews < ActiveRecord::Migration[5.2]
   def change
     create_table :news do |t|
+      t.string :title
+      t.text :link
+      t.text :image
     end
   end
 end
