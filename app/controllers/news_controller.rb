6e896bdd2d18a624e73bd8119862168fa00e6e8f
hirokazu kobayashi <solness.stf@gmail.com>
2021-05-01 23:29:52 +0900

    ニュース一覧から気になるニュースをデータベースに保存する機能

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,10 +3,15 @@ class NewsController < ApplicationController
     #require 'news-api'
     newsapi = News.new("840742eb9a874490b37a04e08b45c848")
     @top_headlines = newsapi.get_top_headlines(language: 'en')
+
+    @news = New.all
   end
 
   def create
     New.create(title: params[:title], link: params[:link], image: params[:image])
     redirect_to news_index_path
   end
+
+
+
 end
