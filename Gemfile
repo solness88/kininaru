d4c35caa05fd06a5c2fd8e516346aaa8bea46076
hirokazu kobayashi <solness.stf@gmail.com>
2021-05-05 23:29:35 +0900

    userコントローラー作成

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'news-api'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'mimemagic', '0.3.7'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_125028) do
+ActiveRecord::Schema.define(version: 2021_05_05_125318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_04_29_125028) do
     t.string "title"
     t.text "link"
     t.text "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
