b90c8c92f1253e1f93f7747196df7fa126c153bd
hirokazu kobayashi <solness.stf@gmail.com>
2021-05-16 10:38:23 +0900

    コメント機能の実装、Ajaxの不具合を解消中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,36 @@
 class CommentsController < ApplicationController
+  before_action :set_news, only: [:create, :edit, :update]
 
   def create
     # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
-    @new = New.find(params[:news_id])
     @comment = @new.comments.build(comment_params)
     # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to news_index_path }
+        format.js { render :index }
       else
         format.html { redirect_to news_index_path, notice: '投稿できませんでした...' }
+      end
+    end
+  end
+
+  def edit
+    @comment = @new.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @new.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit_error }
       end
     end
   end
@@ -18,6 +39,10 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:new_id, :content)
+  end
+
+  def set_news
+    @new = New.find(params[:news_id])
   end
 
 end
