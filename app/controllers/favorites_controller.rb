3a333c0869fe943451df928259bfe69d075df540
hirokazu kobayashi <solness.stf@gmail.com>
2021-05-22 19:11:03 +0900

    いいねボタンをajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,16 @@
 class FavoritesController < ApplicationController
+  #before_action :set_news, only: [:create, :destroy]
 
   def create
     favorite = current_user.favorites.create(new_id: params[:new_id])
-    redirect_to news_index_path, notice: "#{favorite.new.user.name}さんの気になるニュースをお気に入り登録しました"
+    @new = New.find(params[:new_id])
   end
 
   def destroy
+    favorite = Favorite.find(params[:id])
+    @delete_new = favorite.new
+    #@new = New.find(params[:new_id])
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to news_index_path, notice: "#{favorite.new.user.name}さんの気になるニュースをお気に入り解除しました"
   end
 
 end
